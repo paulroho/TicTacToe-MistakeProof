dfdaf2fc9846417c8cce55c2b77e3c2debd192b6
Paul Rohorzka <paul.rohorzka@techtalk.at>
2017-09-27 15:48:08 +0200

    Position can Parse from letter combination

---
+++ Tests/PositionTests.cs
@@ -0,0 +1,17 @@
+﻿using FluentAssertions;
+using TicTacToe.MistakeProof;
+using Xunit;
+
+namespace Tests
+{
+    public class PositionTests
+    {
+        [Fact]
+        public void Parse_HappyPath()
+        {
+            var actual = Position.Parse("m r");
+
+            actual.Should().Be(new Position(Row.Middle, Column.Right));
+        }
+    }
+}
--- TicTacToe-MistakeProof/Position.cs
+++ TicTacToe-MistakeProof/Position.cs
@@ -1,17 +1,42 @@
 using System;
+using System.Linq;
 
 namespace TicTacToe.MistakeProof
 {
     public class Position
     {
         // complete
+
         private readonly Row _row;
+
         private readonly Column _column;
 
         public Position(Row row, Column column)
         {
             _row = row;
             _column = column;
+        }
+
+        public static Position Parse(string text)
+        {
+            var parts = text.Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+            return new Position(ParseRowFromLetter(parts[0]), ParseColumnFromLetter(parts[1]));
+        }
+
+        private static Row ParseRowFromLetter(string letter)
+        {
+            var enumName = Enum.GetNames(typeof(Row)).First(n => n.ToLowerInvariant().StartsWith(letter));
+            Row value;
+            Enum.TryParse(enumName, out value);
+            return value;
+        }
+
+        private static Column ParseColumnFromLetter(string letter)
+        {
+            var enumName = Enum.GetNames(typeof(Column)).First(n => n.ToLowerInvariant().StartsWith(letter));
+            Column value;
+            Enum.TryParse(enumName, out value);
+            return value;
         }
 
         public override bool Equals(object obj)
